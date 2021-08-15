11b381817d87570f5a2b56299b260af0a0aaecb1
Mark Wylde <me@markwylde.com>
2021-08-15 20:09:32 +1000

    Add search term; Fix error guard;

--- content.js
+++ content.js
@@ -1,33 +1,43 @@
-if (document.body.innerText.indexOf("Our systems have detected unusual traffic") !=-1) {
-  const currentUrl = new URL(document.location.href)
-  const url = new URL(currentUrl.searchParams.get('continue'))
-  const query = url.searchParams.get('q');
+function main () {
+  if (document.body.innerText.indexOf('Our systems have detected unusual traffic') > -1) {
+    const currentUrl = new URL(document.location.href);
+    const continueUrl = currentUrl.searchParams.get('continue');
 
-  const dom = document.createElement('div')
-  dom.style.maxWidth = '400px';
-  dom.innerHTML = `
-    <b>Google's being awful again:</b>
-    <p>Try one of these better search engines:</p>
+    if (!continueUrl) {
+      return;
+    }
 
-    <ul>
-      <li>
-        <a href="https://duckduckgo.com/?q=${query}">DuckDuckGo</a>
-      </li>
-      <li>
-        <a href="https://startpage.com/do/metasearch.pl?query=${query}">StartPage</a>
-      </li>
-      <li>
-        <a href="https://www.bing.com/search?q=${query}">Bing</a>
-      </li>
-      <li>
-        <a href="https://search.brave.com/search?q=${query}">Brave</a>
-      </li>
-    </ul>
+    const url = new URL(continueUrl);
+    const query = url.searchParams.get('q');
 
-    <p>
-      Or, if you really need to, you can still solve the captcha below.
-    </p>
-  `
+    const dom = document.createElement('div');
+    dom.style.maxWidth = '400px';
+    dom.innerHTML = `
+      <b>Google's being awful again:</b>
+      <p><strong>Your search term was:</strong> ${query}</p>
+      <p>Try one of these better search engines:</p>
 
-  document.body.prepend(dom);
+      <ul>
+        <li>
+          <a href="https://duckduckgo.com/?q=${query}">DuckDuckGo</a>
+        </li>
+        <li>
+          <a href="https://startpage.com/do/metasearch.pl?query=${query}">StartPage</a>
+        </li>
+        <li>
+          <a href="https://www.bing.com/search?q=${query}">Bing</a>
+        </li>
+        <li>
+          <a href="https://search.brave.com/search?q=${query}">Brave</a>
+        </li>
+      </ul>
+
+      <p>
+        Or, if you really need to, you can still solve the captcha below.
+      </p>
+    `;
+
+    document.body.prepend(dom);
+  }
 }
+main();
